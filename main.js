cc87179afebfe5ab17a967d90d8ed1d44073ed1d
Lance Campbell <lkcampbell@gmail.com>
2013-08-31 10:29:57 -0700

    Fix issue #4

--- main.js
+++ main.js
@@ -50,6 +50,10 @@ define(function (require, exports, module) {
         
         command = document.getRange({line: pos.line, ch: start}, {line: pos.line, ch: end});
         
+        if (command.match(/lorem/)) {
+            command = command.substring(command.match(/lorem/).index);    
+        }
+        
         return ((command.split("_")[0] === "lorem") ? command : "");
     }
     
